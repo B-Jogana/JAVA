28468357562236bc19b608864013e3fc484a307c
B-Jogana <193931131+B-Jogana@users.noreply.github.com>
2025-11-08 18:56:27 +0100

    Lesson_2.java

--- First_Week/First_Week/Lesson_2.java
+++ First_Week/First_Week/Lesson_2.java
@@ -20,19 +20,3 @@ public class Lesson_2 {
     
 }
 
-/* 
- import java.util.Scanner;  // Import the Scanner class
-
-public class Lesson_2 {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in); // Create Scanner object
-
-        System.out.print("Enter your name: ");
-        String name = input.nextLine(); // Read a line of text
-
-        System.out.print("Enter your age: ");
-        int age = input.nextInt(); // Read an integer
-
-        System.out.println("Hello, " + name + "! You are " + age + " years old.");
-    }
-} */
