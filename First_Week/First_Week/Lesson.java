37bb06530fb2513aa0f4ae9bd2eb654da7dd63f9
B-Jogana <193931131+B-Jogana@users.noreply.github.com>
2025-11-08 14:25:47 +0100

    Initial commit

---
+++ First_Week/First_Week/Lesson.java
@@ -0,0 +1,40 @@
+import java.io.StringReader;
+
+public class Lesson {
+    public static void main(String[] args) {
+      /*   String firstName = " Bashir";
+        String LastName = "Ali";
+        String fullName = firstName + " " + LastName;
+        String dept = "Software Engineering ";
+        String hobby = "Reading & Coding ";
+        int level = 200;
+
+        System.out.println("Full Name: " + fullName);
+        System.out.println("Department: " + dept);
+        System.out.println("Level " + level);
+        System.out.println("Hobby:" + hobby); */
+
+       /*  int num_1= 2;
+        int num_2 = 3;
+        int add = num_1 + num_2;
+        int minus = num_2 - num_1;
+        int mult = num_1 * num_2;
+        float div = (float) num_1/num_2;
+
+        System.out.println("Addtion of " + num_1  + " and "+ num_2+ " is : " + add);
+        System.out.println("Subtraction of " + num_2  + " and "+ num_1+ " is : " + minus);
+        System.out.println("Multiplication of " + num_1  + " and "+ num_2+ " is : " + mult);
+        System.out.println("Division of  " + num_1  + " and "+ num_2+ " is : " + div); */
+
+        double celcius = 34;
+        double fahrenheit = (celcius*9/5) + 32;
+        System.out.println(celcius + " celcius  make " + fahrenheit + " fahrenheit"); 
+
+
+
+
+
+
+
+    }
+}
