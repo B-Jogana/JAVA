d1a60a8d9126cf2b1b7e8b8878a1e6ac87bdc247
B-Jogana <193931131+B-Jogana@users.noreply.github.com>
2025-11-27 14:09:37 +0100

    Add Lesson_1 file to fistWeek folder

---
+++ firstWeek/Lesson_1.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class Lesson_1{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        //A program to print out Student Data
+     /*    String name =" Bashir" ;
+        String dept = "SoFtware Department ";
+        int Level = 200;
+        String Hobby = "Programming";
+
+        System.out.println(name + "\n" + dept + "\n" + Level + "\n" + Hobby  ); */
+
+
+        // Simple calaculation program
+
+
+        /* int num_1 = 10;
+        int num_2 = 5;
+        //int sum = num_1 + num_1;
+
+        System.out.println("Sum Of " + num_1 + " and " + num_2 + " = " + num_1+num_2);
+        System.out.println("Subracion  Of " + num_1 + " from " + num_2 + " = " + (num_1-num_2));
+        System.out.println("Multiplication  Of " + num_1 + " and " + num_2 + " = " + num_1*num_2);
+        System.out.println("Division  Of " + num_1 + " and " + num_2 + " = " + num_1/num_2); */
+
+        //A simple program for conversion of cecius 
+
+
+       /*  double celcius = 45;
+        double  fahrenheit = (celcius * 9/5  ) + 32;
+
+        System.out.println(celcius + "C is equivalence to " + fahrenheit + "F"); */
+
+        // a program to collect input from user 
+
+       /*  System.out.println("Enter your Name: ");
+        String name = input.nextLine();
+
+
+        System.out.println("Enter your age:");
+        int age = input.nextInt();input.nextLine();
+
+        System.out.println("Your name is " + name + "! you are " + age + "years old"); */
+
+
+
+    }
+
+
+
+}
